9371b8b367e0b854090af7e12b3c65fe9780a96a
Qasim <qasem.yousifi20@gmail.com>
2022-05-25 17:43:46 +0430

    Updated filter options

--- pages/Category/widgets/filter_modal_bottom_sheet.dart
+++ pages/Category/widgets/filter_modal_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FilterModalBottomSheet extends StatelessWidget {
   const FilterModalBottomSheet({Key? key}) : super(key: key);
@@ -8,57 +9,95 @@ class FilterModalBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Wrap(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              width: 100,
-              padding: EdgeInsets.all(4),
-              child: InkWell(
-                child: Icon(Icons.close),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-            Text('Filter',textAlign: TextAlign.center,),
-            Container(
-              width: 100,
-              alignment: Alignment.centerRight,
-              child: InkWell(
-                child: Text('Reset',textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.blue),),
-                onTap: (){
-                },
-                ),
-              )
-            ]
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Text('Price Range', style: TextStyle(fontSize: 20),),
-          ),
-          Container(
-            width: double.infinity,
-            child: Wrap(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Wrap(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  width: ((size.width/2) - 55),
-                  decoration: BoxDecoration(
+              Container(
+                alignment: Alignment.centerLeft,
+                width: 100,
+                padding: EdgeInsets.all(4),
+                child: InkWell(
+                  child: Icon(Icons.close),
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              Text('Filter',textAlign: TextAlign.center,),
+              Container(
+                width: 100,
+                alignment: Alignment.centerRight,
+                child: InkWell(
+                  child: Text('Reset',textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue),),
+                  onTap: (){
+                  },
+                  ),
+                )
+              ]
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text('Price Range', style: TextStyle(fontSize: 20),),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 15),
+              width: double.infinity,
+              child: Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                alignment: WrapAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: ((size.width/2) - 55),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        border: Border.all(color: Colors.black26 ),
+                    ),
+                    child: TextField(
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(7),
+                        hintText: 'Minimum',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: ((size.width/2) - 55),
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(4),
                       border: Border.all(color: Colors.black26 ),
+                    ),
+                    child: TextField(
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(7),
+                        hintText: 'Minimum',
+                      ),
+                    ),
                   ),
 
-                )
-              ],
+                ],
+              ),
             ),
-          )
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text('Filter Items', style: TextStyle(fontSize: 20),),
+            ),
+            Container(
+              child: FilterList(
+                onSelected: (selected) => print(selected);
+              ),
+            )
 
-        ],
+          ],
+        ),
       ),
     );
   }
