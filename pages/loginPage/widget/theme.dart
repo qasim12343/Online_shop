170898139ad7c2e8296247d3365ed5779e2ff8e7
Qasim <qasem.yousifi20@gmail.com>
2022-05-31 00:09:58 +0430

    Another decoration added

--- pages/loginPage/widget/theme.dart
+++ pages/loginPage/widget/theme.dart
@@ -17,7 +17,45 @@ class Them{
       focusedErrorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.red, width: 2.0)),
     );
   }
+  BoxDecoration inputBoxDecorationShaddow() {
+    return BoxDecoration(boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.1),
+        blurRadius: 20,
+        offset: const Offset(0, 5),
+      )
+    ]);
+  }
 
-
+  BoxDecoration buttonBoxDecoration(BuildContext context, [String color1 = "", String color2 = ""]) {
+    Color c1 = Theme
+        .of(context)
+        .primaryColor;
+    Color c2 = Theme
+        .of(context)
+        .accentColor;
+    if (color1.isEmpty == false) {
+      c1 = HexColor(color1);
+    }
+    if (color2.isEmpty == false) {
+      c2 = HexColor(color2);
+    }
+    return BoxDecoration(
+      boxShadow: [
+        BoxShadow(color: Colors.black26, offset: Offset(0, 4), blurRadius: 5.0)
+      ],
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        stops: [0.0, 1.0],
+        colors: [
+          c1,
+          c2,
+        ],
+      ),
+      color: Colors.deepPurple.shade300,
+      borderRadius: BorderRadius.circular(30),
+    );
+  }
 
 }
