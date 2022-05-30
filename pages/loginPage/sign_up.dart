df3c7cb9a6c5df7eb2d4eccccaad2071c76d580f
Qasim <qasem.yousifi20@gmail.com>
2022-05-31 00:39:26 +0430

    Container - Column - Form - GestureDetector and others implementations in SignUp page

--- pages/loginPage/sign_up.dart
+++ pages/loginPage/sign_up.dart
@@ -1,15 +1,89 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class SignUp extends StatefulWidget {
-  const SignUp({Key? key}) : super(key: key);
-
-  @override
-  _SignUpState createState() => _SignUpState();
+void main(){
+  runApp(MaterialApp(
+    home: SignUp(),
+    showSemanticsDebugger: false,
+  ));
 }
 
-class _SignUpState extends State<SignUp> {
+class SignUp extends  StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  State<StatefulWidget> createState() {
+    return _SignUp();
   }
 }
+
+
+class _SignUp extends State<SignUp>{
+
+  final _formKey = GlobalKey<FormState>();
+  bool checkedValue = false;
+  bool checkboxValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(25, 50, 25, 10),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          child: Stack(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  border: Border.all(
+                                      width: 5, color: Colors.white),
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black12,
+                                      blurRadius: 20,
+                                      offset: const Offset(5, 5),
+                                    ),
+                                  ],
+                                ),
+                                child: Icon(
+                                  Icons.person,
+                                  color: Colors.grey.shade300,
+                                  size: 80.0,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.fromLTRB(80, 80, 0, 0),
+                                child: Icon(
+                                  Icons.add_circle,
+                                  color: Colors.grey.shade700,
+                                  size: 25.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),),
+
+                ],
+              )
+            )
+          ],
+        ),
+        ),
+    );
+  }
+}
+
