0f6352f3aa6348f57bce580ccf0c04881fa9dcb0
Qasim <qasem.yousifi20@gmail.com>
2022-06-28 17:24:01 +0430

    debugged the Item and user classes for json.

--- lib/data/item.dart
+++ lib/data/item.dart
@@ -13,27 +13,40 @@ class Item {
   bool isMine;
   List<Color>? colors  = [Colors.black12];
 
-  Map toJson() => {
-    'name': name,
-    'sellerName': sellerName,
-    'imagePath': imagePath,
-    'discount': discount,
-    'originalPrice': originalPrice,
-    'rating': rating,
-    'isMine':isMine,
-    'count':count,
-  };
+  Map toJson(){
+    List<int>? colors =
+    this.colors != null? this.colors!.map((e) => e.value).toList(): [Colors.black12.value];
+    return {
+      'name': name,
+      'sellerName': sellerName,
+      'imagePath': imagePath,
+      'discount': discount,
+      'originalPrice': originalPrice,
+      'rating': rating,
+      'isMine':isMine,
+      'count':count,
+      'colors':colors,
+    };
+  }
   factory Item.fromJson(dynamic json) {
+    var colorValue = json['colors'] as List;
+    List<Color> colors = [];
+    colorValue.forEach((element) {
+      colors.add(Color(element));
+    });
+
     return Item(name: json['name'] as String, imagePath: json['imagePath'] as String,
       discount: json['discount'] as int , originalPrice: json['originalPrice'] as double, rating: json['rating'] as double,
       isMine: json['isMine'] != null ? json['isMine'] as bool: false,
       count: json['count'] != null ? json['count'] as int: 10,
       sellerName: json['sellerName'] != null ? json['sellerName'] as String: 'NamaKala',
+      colors: json['colors'] !=null? colors : [Colors.black12],
     );
   }
 
   Item({required this.name, required this.imagePath, required this.discount, required this.originalPrice,
       required this.rating,this.isMine = false, this.colors, this.sellerName = 'NamaKala', this.count = 10});
+
   double get Price{
     return discount != 0 ? (originalPrice -(originalPrice*discount/100)):(originalPrice);
   }
--- lib/data/user.dart
+++ lib/data/user.dart
@@ -14,36 +14,39 @@ class User{
   User({required this.firstName, this.lastName = '',required this.password, required this.email, required this.phoneNumber});
 
   Map toJson(){
-    List<Map>? purchases = this.purchases != null? this.purchases!.map((e) => e.toJson()).toList(): null;
-    List<Map>? favorites = this.favorites != null? this.favorites!.map((e) => e.toJson()).toList(): null;
-    List<Map>? myProducts = this.myProducts != null? this.myProducts!.map((e) => e.toJson()).toList(): null;
-    return{
-      'firsName':firstName,
-      'lastName':lastName,
-      'password': password,
-      'email': email,
-      'phoneNumber':phoneNumber,
-      'purchases': purchases,
-      'favorite': favorites,
-      'myProducts': myProducts,
-    };
+      List<Map>? purchases =
+      this.purchases != null? this.purchases!.map((e) => e.toJson()).toList(): null;
+      List<Map>? favorites =
+      this.favorites != null? this.favorites!.map((e) => e.toJson()).toList(): null;
+      List<Map>? myProducts =
+      this.myProducts != null? this.myProducts!.map((e) => e.toJson()).toList(): null;
+      return{
+        'firstName':firstName,
+        'lastName':lastName,
+        'password': password,
+        'email': email,
+        'phoneNumber':phoneNumber,
+        'purchases': purchases,
+        'favorites': favorites,
+        'myProducts': myProducts,
+      };
 
-  }
-  factory User.fromJson(dynamic json) {
-    var purchasesJsonList = json['purchases'] as List;
-    var favoritesJsonList = json['favorites'] as List;
-    var myProductsJsonList = json['myProducts'] as List;
-    List<Item> purchases = purchasesJsonList.map((e) => Item.fromJson(e)).toList();
-    List<Item> favorites = favoritesJsonList.map((e) => Item.fromJson(e)).toList();
-    List<Item> myProducts = myProductsJsonList.map((e) => Item.fromJson(e)).toList();
-    User user =  User(firstName: json['firstName'] as String, password: json['password'] as String,
-      email: json['email'] as String, phoneNumber: json['phoneNumber'] as String,
-    );
-    user.myProducts = json['myProducts'] != null ? myProducts: [];
-    user.purchases = json['favorites'] != null ? favorites: [];
-    user.purchases = json['purchases'] != null ? purchases: [];
-    return user;
-  }
+    }
+    factory User.fromJson(dynamic json) {
+      var purchasesJsonList = json['purchases'] as List;
+      var favoritesJsonList = json['favorites'] as List;
+      var myProductsJsonList = json['myProducts'] as List;
+      List<Item> purchases = purchasesJsonList.map((e) => Item.fromJson(e)).toList();
+      List<Item> favorites = favoritesJsonList.map((e) => Item.fromJson(e)).toList();
+      List<Item> myProducts = myProductsJsonList.map((e) => Item.fromJson(e)).toList();
+      User user =  User(firstName: json['firstName'] as String, password: json['password'] as String,
+          email: json['email'] as String, phoneNumber: json['phoneNumber'] as String,
+      );
+      user.myProducts = json['myProducts'] != null ? myProducts: [];
+      user.purchases = json['favorites'] != null ? favorites: [];
+      user.purchases = json['purchases'] != null ? purchases: [];
+      return user;
+    }
 
   @override
   bool operator ==(Object other) =>
